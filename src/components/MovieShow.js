1795b1e85dd9355ef964cf511c1a9659b5f69200
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-29 15:40:32 +0300

    Updated Code

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+import { useParams } from "react-router-dom"
+
+export default function MovieShow({ movies }) {
+
+    const params = useParams()
+    console.log(params)
+    return (
+        <div>
+        <h3>{movies[params.movieId].title}</h3>
+        </div>
+    )
+}
